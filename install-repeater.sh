f9632aa6fcd3f6e1007fcb0a02fc867cfe96f565
zhouhongxiang <hongxiang.zhou@transsion.com>
2024-11-08 14:26:43 +0800

    feature：sandbox文件

---
+++ install-repeater.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+typeset SANDBOX_HOME=${HOME}/sandbox
+
+typeset MODULE_HOME=${HOME}/.sandbox-module
+
+# exit shell with err_code
+# $1 : err_code
+# $2 : err_msg
+exit_on_err()
+{
+    [[ ! -z "${2}" ]] && echo "${2}" 1>&2
+    exit ${1}
+}
+
+main(){
+    echo "======  begin to install sandbox and repeater module       ======";
+    echo "======  step 0 begin to download sandbox package           ======";
+    curl -L -s https://github.com/alibaba/jvm-sandbox-repeater/releases/download/v1.0.0/sandbox-1.3.3-bin.tar | tar xz -C ${HOME} || exit_on_err 1 "extract sandbox failed"
+    echo "======  step 1 begin to download repeater module package   ======";
+    if [ ! -d ${MODULE_HOME} ]; then
+        mkdir -p ${MODULE_HOME} || exit_on_err 1 "permission denied mkdir ${MODULE_HOME}"
+    fi
+    curl -L -s https://gitlab.shalltry.com/hongxiang.zhou/jvm-sandbox-repeater/-/raw/master/sandbox-module.tar | tar xz -C ${MODULE_HOME} || exit_on_err 1 "extract repeater failed"
+    echo "======                 install finished                    ======";
+}
+
+main
